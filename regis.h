281cb04c7297e71bd72b63203b21dbc3ee328c81
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2022-12-27 13:43:05 +0700

    Update regis.h

--- regis.h
+++ regis.h
@@ -5,15 +5,19 @@
 using namespace std;
 
 struct mhs {
-  
+  string nama,nim;
+  int nim,semester;
 };
 
 struct beasiswa {
-  
+  string nama,instansi,benefit;
+  string syarat;
 };
 
 struct univ {
-  
+  string nama;
+  bool statusNegeri;
+  char akreditasi;
 };
 
 #endif // REGIS_H_INCLUDED
