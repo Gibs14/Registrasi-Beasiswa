88469865b3c68f4317925bbcb88c29ff949edd2d
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2022-12-27 13:54:17 +0700

    Add data in header

--- regis.h
+++ regis.h
@@ -2,22 +2,59 @@
 #define REGIS_H_INCLUDED
 
 #include <iostream>
+
+#define info(P) (P)->info
+#define nextUniv(P) (P)->nextUniv
+#define nextMhs(P) (P)->nextMhs
+#define nextBeasiswa(P) (P)->nextBeasiswa
+
 using namespace std;
 
 struct mhs {
   string nama,nim;
-  int nim,semester;
+  int ipk,semester;
 };
 
 struct beasiswa {
   string nama,instansi,benefit;
-  string syarat;
+  string syarat; //string ati mau di how-in?
 };
 
 struct univ {
   string nama;
   bool statusNegeri;
   char akreditasi;
+};
+
+typedef struct elmMhs *adrMhs;
+typedef struct elmUniv *adrUniv;
+typedef struct elmBeasiswa *adrBeasiswa;
+
+struct elmMhs {
+    mhs info;
+    adrUniv nextUniv;
+    adrBeasiswa nextBeasiswa;
+    adrMhs nextMhs;
+};
+
+struct elmUniv {
+    beasiswa info;
+    adrUniv nextUniv;
+};
+
+struct elmBeasiswa {
+    beasiswa info;
+    adrBeasiswa nextBeasiswa;
+};
+
+struct listMhs {
+    adrMhs first;
+    //adrMhs last; ::Mau Pake Last?::
+};
+
+struct listBeasiswa {
+    adrBeasiswa first;
+    //adrMhs last; ::Mau Pake Last?::
 };
 
 #endif // REGIS_H_INCLUDED
