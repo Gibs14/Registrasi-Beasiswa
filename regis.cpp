b32227454fbb2b2acea3639438a698097958585a
Zen <109281756+Gibs14@users.noreply.github.com>
2022-12-30 00:53:38 +0700

    Update regis.cpp

---
+++ regis.cpp
@@ -0,0 +1,51 @@
+#include "regis.h"
+
+void createListMhs(listMhs &M) {
+    first(M) = nil;
+}
+
+void createListUniv(listUniv &U) {
+    first(U) = nil;
+}
+
+void createListBeasiswa(listBeasiswa &B) {
+    first(B) = nil;
+}
+
+adrUniv createAdrUniv(string nama, bool statusNegri, char Akreditasi) {
+
+}
+
+
+infoSyarat inputSyaratBeasiswa(float ipkMin, int semester, bool statusNegeri, char akreditasi) {
+
+}
+
+adrBeasiswa createAdrBeasiswa(string nama, string instansi, string benefit, infoSyarat syarat) {
+
+}
+
+
+infoMhs inputDataMhs(string nama, string nim, float ipk, int semester) {
+
+}
+
+//DIsini kudunya ada func/proc buat si adrChildBeasiswa, atau kalo ada cara lbh better sok aja (janlup jelasin wkwk)
+//adrUniv gaperlu lg soalnya udh di atas, jd sisa yg kata adrChildBeasiswa aj yg w bingung (yg udh w mention di atas jg)
+
+adrMhs createAdrMhs(infoMhs infoM, adrUniv asalUniv, adrChildBeasiswa inBeasiswa) {
+
+}
+
+
+void addUniv(listUniv &U, adrUniv p) {
+
+}
+
+void addBeasiswa(listBeasiswa &B, adrBeasiswa p) {
+
+}
+
+void addMhs(listMhs &M, adrMhs p) {
+
+}
