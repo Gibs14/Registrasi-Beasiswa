bf2979cc8444a18d8dd54ac63ddfea03a176faf6
Zen <109281756+Gibs14@users.noreply.github.com>
2022-12-30 01:06:31 +0700

    Update regis.cpp

--- regis.cpp
+++ regis.cpp
@@ -24,16 +24,33 @@ adrUniv createAdrUniv(string nama, bool statusNegri, char Akreditasi) {
 
 
 infoSyarat inputSyaratBeasiswa(float ipkMin, int semester, bool statusNegeri, char akreditasi) {
-
+    infoSyarat p;
+    p.ipkMin = ipkMin;
+    p.semester = semester;
+    p.statusNeger = statusNegeri;
+    p.akreditas = akreditasi;
+    return p;
 }
 
 adrBeasiswa createAdrBeasiswa(string nama, string instansi, string benefit, infoSyarat syarat) {
-
+    adrBeasiswa p;
+    p = new elmBeasiswa;
+    info(p).nama = nama;
+    info(p).instansi = instansi;
+    info(p).benefit = benefit;
+    info(p).infoSyarat = infoSyarat;
+    next(p) = nil;
+    return p;
 }
 
 
 infoMhs inputDataMhs(string nama, string nim, float ipk, int semester) {
-
+    infoMhs p;
+    p.nama = nama;
+    p.nim = niml
+    p.ipk = ipk;
+    p.semester = semester;
+    return p;
 }
 
 //DIsini kudunya ada func/proc buat si adrChildBeasiswa, atau kalo ada cara lbh better sok aja (janlup jelasin wkwk)
@@ -45,13 +62,37 @@ adrMhs createAdrMhs(infoMhs infoM, adrUniv asalUniv, adrChildBeasiswa inBeasiswa
 
 
 void addUniv(listUniv &U, adrUniv p) {
-
+    if (first(U) == nil) {
+        first(U) = p;   
+    } else {
+        adrUniv u = first(U);   
+        while (next(u) != nil) {
+            u = next(u);   
+        }
+        next(u) = p;
+    }
 }
 
 void addBeasiswa(listBeasiswa &B, adrBeasiswa p) {
-
+    if (first(B) == nil) {
+        first(B) = p;   
+    } else {
+        adrUniv b = first(B);   
+        while (next(b) != nil) {
+            b = next(b);   
+        }
+        next(b) = p;
+    }
 }
 
 void addMhs(listMhs &M, adrMhs p) {
-
+    if (first(M) == nil) {
+        first(M) = p;   
+    } else {
+        adrUniv m = first(M);   
+        while (next(m) != nil) {
+            m = next(m);   
+        }
+        next(m) = p;
+    }
 }
